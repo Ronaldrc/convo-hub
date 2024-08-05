557b24f6b1f690daab868d5de192515161c8ad30
Ron Chim <ronrchim@gmail.com>
2024-08-05 13:10:57 -0400

    Create test route for api

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,16 +12,13 @@ CORS(app)
 
 db = SQLAlchemy(app)
 
-with app.app_context():
-    db.reflect()
-
 # Define model
-class Booking(db.Model):
-    __tablename__ = 'booking'
+class UserMessage(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Integer)
+    username = db.Column(db.String(40))
     message = db.Column(db.String(400), nullable=False)
-    time_message_sent = db.Column(db.DateTime, nullable=False)
+    time_message_sent = db.Column(db.DateTime(timezone=True), nullable=False)
 
     def json(self):
         return {
@@ -31,12 +28,53 @@ class Booking(db.Model):
             'time_message_sent' : self.time_message_sent
         }
 
-# Define api routes
-@app.route("/", methods=['GET'])
+# Create all tables
+with app.app_context():
+    db.create_all()
+    
+####################
+## Define api routes
+####################
+
+@app.route("/api/test", methods=['GET'])
 def test():
     try:
-        return("testing api route! - success!", 200)
+        return("SUCCESS testing api route!", 200)
     except Exception as e:
         return(f"FAILED test api route - {e}", 500)
+    
+@app.route('/api/test', methods=['POST'])
+def post_username_message_time():
+    try:
+        data = request.get_json()
+        new_user = UserMessage(
+            username = data['username'],
+            message = data['message'],
+            time_message_sent = ['time_message_sent'],
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({
+            'new_user' : new_user,
+            'message' : 'success'
+        }, 200)
+    except Exception as e:
+        return(f"Failed to post username, message, and time - {e}", 500)
 
-app.run(host=os.getenv('HOST_IP'))
+@app.route('/api/', methods=['GET'])
+def get_username_message_time():
+    try:
+        response = db.Query.filter(UserMessage.username == 'kingjames').all()
+        if response:
+            return jsonify({
+                'get_username_message' : 'successfully received',
+                'response' : response
+            }, 200)
+        if not response:
+            return jsonify({
+                'get_username_message' : 'no username found'
+            }, 200)
+    except Exception as e:
+        return(f"Failed to get username, message, and time sent - {e}")
+
+app.run(host=os.getenv('HOST_IP'), debug=True)
