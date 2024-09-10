fa52abde2cce199f1cfc101aa4d296450366cfe9
Ron Chim <ronrchim@gmail.com>
2024-09-10 04:15:29 -0400

    Fix grammar

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Chat = ({usernames, messages}) => {
     return (
-        <div className="textbox">
+        <div className="chatbox-messages">
             {messages.map((message, index) => (
             <div key={index}>
                 <strong>{usernames[index]}:</strong> {message}
